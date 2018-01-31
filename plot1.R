f5c85babb1ada855164197cfa7bfc2569ba18d2c
Trever Sanborn <sanborn.trev@gmail.com>
2018-01-30 20:43:51 -0800

    finished plot1

--- plot1.R
+++ plot1.R
@@ -1,3 +1,19 @@
 plot1 <- function() {
-    hpc <- read.table("../household_power_consumption.txt", header = TRUE, sep = ";")
+    library(dplyr)
+    library(lubridate)
+    
+    # loads the initial data set
+    hpc <- read.table("../datasets/household power consumption/household_power_consumption.txt",
+                      header = TRUE, sep = ";")
+    
+    # filters the data set
+    hpc2 <- filter(hpc, dmy(Date) == "2007-02-01" | dmy(Date) == "2007-02-02")
+    
+    # creates the histogram pdf
+    png(file = 'plot1.png')
+    hist(as.numeric(as.character(hpc2$Global_active_power)),
+         col = 'red',
+         xlab = 'Global Active Power (kilowatts)',
+         main = 'Global Active Power')
+    dev.off()
 }
